52f1b612090a6f594a61672df1d952b5367c55f6
evilPaprika <ivil.paprika@gmail.com>
2018-11-03 20:40:48 +0500

    версия один

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
@@ -12,32 +12,58 @@ const isStar = true;
  */
 function getEmitter() {
     return {
+        listeners: new Proxy(
+            {},
+            {
+                get: (target, name) =>
+                    target.hasOwnProperty(name) ? target[name] : (target[name] = [])
+            }
+        ),
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            this.listeners[event].push({ handler: handler.bind(context), context });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (const [index, callback] of this.listeners[event].entries()) {
+                if (callback.context === context) {
+                    this.listeners[event].splice(index, 1);
+
+                    return this;
+                }
+            }
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            for (const callback of this.listeners[event]) {
+                callback.handler();
+            }
+            const removedAfterDot = event.replace(/\.[^.]+$/, '');
+            if (removedAfterDot !== event) {
+                this.emit(removedAfterDot);
+            }
+
+            return this;
         },
 
         /**
